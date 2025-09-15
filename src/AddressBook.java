13ce052d48ac699b7b906a9b85be5033f2b3d4f4
Mahdi Bouakline <bouaklinemahdi@gmail.com>
2025-09-15 17:37:38 -0400

    First change

---
+++ src/AddressBook.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+
+    // A collection to hold BuddyInfo objects
+    private List<BuddyInfo> buddies;
+
+    // Constructor initializes the collection
+    public AddressBook() {
+        buddies = new ArrayList<>();
+    }
+
+    // Adds a BuddyInfo to the address book
+    public void addBuddy(BuddyInfo buddy) {
+        if (buddy != null) {
+            buddies.add(buddy);
+        }
+    }
+
+    // Removes a BuddyInfo at a specific index
+    public void removeBuddy(int index) {
+        if (index >= 0 && index < buddies.size()) {
+            buddies.remove(index);
+        }
+    }
+
+    // Example main method to test functionality
+    public static void main(String[] args) {
+        AddressBook book = new AddressBook();
+        BuddyInfo buddy = new BuddyInfo("Homer", "742 Evergreen Terrace", "555-1234");
+        book.addBuddy(buddy);
+        book.removeBuddy(0);
+        System.out.println("Address Book");
+    }
+
+}
