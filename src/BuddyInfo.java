9b60917e569080954db04a9834a05faf1f2813ec
Mahdi Bouakline <bouaklinemahdi@gmail.com>
2025-09-19 13:49:15 -0400

    Added and removed buddy.

---
+++ src/BuddyInfo.java
@@ -0,0 +1,32 @@
+public class BuddyInfo {
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public BuddyInfo(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public BuddyInfo() {
+        this("Homer", "Homer Street", "111-1111");
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("Homer", "742 Evergreen Terrace", "555-1234");
+        System.out.println("Hello " + buddy.getName());
+    }
+
+}
